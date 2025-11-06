b0681cabec44db8cf4701e03a43cc7b8b936e2d7
kleal <kathya.leal@catolica.edu.sv>
2025-11-06 01:12:32 -0600

    comentarios agregados

--- src/main/java/com/example/centinela_api/controller/EmergenciaController.java
+++ src/main/java/com/example/centinela_api/controller/EmergenciaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
 @RequestMapping("/api/emergencias")
 public class EmergenciaController {
 
--- src/main/java/com/example/centinela_api/controller/ReporteController.java
+++ src/main/java/com/example/centinela_api/controller/ReporteController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true")
 @RequestMapping("/api/reportes")
 public class ReporteController {
 
